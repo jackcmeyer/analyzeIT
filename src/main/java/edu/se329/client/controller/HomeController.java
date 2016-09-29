89a28d5839901d234b983d9507e3df5886e16697
Jack Meyer <jackmeyerdev@gmail.com>
2016-09-28 21:26:06 -0500

    Twitter Example

--- src/main/java/edu/se329/client/controller/HomeController.java
+++ src/main/java/edu/se329/client/controller/HomeController.java
@@ -1,18 +1,42 @@
 package edu.se329.client.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import javax.inject.Inject;
 
-/**
- * @author Jack Meyer jackmeyerdev@gmail.com
- *
- * Controller for the home page.
- */
+import org.springframework.social.connect.ConnectionRepository;
+import org.springframework.social.twitter.api.CursoredList;
+import org.springframework.social.twitter.api.Twitter;
+import org.springframework.social.twitter.api.TwitterProfile;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 @Controller
+@RequestMapping("/")
 public class HomeController {
 
-    @RequestMapping("/")
-    public String home() {
-        return "index"; // returns the ../../resoures/templates/index.html
+    private Twitter twitter;
+
+    private ConnectionRepository connectionRepository;
+
+    @Inject
+    public HomeController(Twitter twitter, ConnectionRepository connectionRepository) {
+        this.twitter = twitter;
+        this.connectionRepository = connectionRepository;
     }
+
+    @RequestMapping(method=RequestMethod.GET)
+    public String helloTwitter(Model model) {
+        if (connectionRepository.findPrimaryConnection(Twitter.class) == null) {
+            return "redirect:/connect/twitter";
+        }
+
+        CursoredList<TwitterProfile> friends = twitter.friendOperations().getFriends();
+        for(TwitterProfile profile : friends) {
+            System.out.println("Name: " + profile.getName() + ", Username: " + profile.getScreenName());
+        }
+
+        return "index";
+    }
+
 }
