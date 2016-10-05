fe0a56cb9f0f3e26b22f2a7a0cfb866edb6d0ec9
Jack Meyer <jackmeyerdev@gmail.com>
2016-10-05 10:02:05 -0500

    Rough sketch of getting tweets from a user

--- src/main/java/edu/se329/client/controller/HomeController.java
+++ src/main/java/edu/se329/client/controller/HomeController.java
@@ -4,12 +4,15 @@ import javax.inject.Inject;
 
 import org.springframework.social.connect.ConnectionRepository;
 import org.springframework.social.twitter.api.CursoredList;
+import org.springframework.social.twitter.api.Tweet;
 import org.springframework.social.twitter.api.Twitter;
 import org.springframework.social.twitter.api.TwitterProfile;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
@@ -31,10 +34,17 @@ public class HomeController {
             return "redirect:/connect/twitter";
         }
 
-        CursoredList<TwitterProfile> friends = twitter.friendOperations().getFriends();
-        for(TwitterProfile profile : friends) {
-            System.out.println("Name: " + profile.getName() + ", Username: " + profile.getScreenName());
+
+        List<Tweet> tweets = twitter.timelineOperations().getUserTimeline("FoxNews", 200);
+
+        for(Tweet tweet : tweets) {
+            tweet.
+            System.out.println(tweet.getText());
+            System.out.println();
         }
+
+        System.out.println(tweets.size());
+
 
         return "index";
     }
